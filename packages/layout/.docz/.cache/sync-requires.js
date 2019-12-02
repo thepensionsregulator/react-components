7e477e32ea16a74eea33127f4bdc7a41eba0d894
Alekna <David.Alekna@tpr.gov.uk>
2019-12-02 11:23:50 +0000

    docz

---
+++ packages/layout/.docz/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\.docz\\.cache\\dev-404-page.js"))),
+  "component---readme-md": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\README.md"))),
+  "component---src-components-header-header-mdx": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\src\\components\\header\\header.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\.docz\\src\\pages\\404.js")))
+}
+
--- packages/layout/src/components/header/__tests__/header.spec.tsx
+++ packages/layout/src/components/header/__tests__/header.spec.tsx
@@ -4,8 +4,8 @@ import { render } from '@testing-library/react';
 import { Header } from '../';
 
 describe('Header', () => {
-  test('it renders correctly', () => {
-    const { getByText } = render(<Header />);
-    expect(getByText('header')).toBeInTheDocument();
-  });
+	test('it renders correctly', () => {
+		const { getByText } = render(<Header title="TPR" />);
+		expect(getByText('TPR')).toBeInTheDocument();
+	});
 });
