6544ac675f42413d33d771f6bb6cf126e6fd2a44
Alekna <David.Alekna@tpr.gov.uk>
2019-12-10 12:06:35 +0000

    docz update

--- packages/layout/.docz/.cache/sync-requires.js
+++ packages/layout/.docz/.cache/sync-requires.js
@@ -6,8 +6,8 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\.docz\\.cache\\dev-404-page.js"))),
-  "component---readme-md": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\README.md"))),
   "component---src-components-header-header-mdx": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\src\\components\\header\\header.mdx"))),
+  "component---readme-md": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\README.md"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\aleknad\\Development\\tpr\\react-components\\packages\\layout\\.docz\\src\\pages\\404.js")))
 }
 
