d03e75d45da314b71ad12469945db519b10e0226
David Alekna <david.alekna@tpr.gov.uk>
2020-03-06 12:02:15 +0000

    exporting all components

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -2,3 +2,5 @@ export * from './utils';
 export * from './components/text';
 export * from './components/checkbox';
 export * from './components/radio';
+export * from './components/date';
+export * from './components/select';
