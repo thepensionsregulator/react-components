6ffb17f9835b0a0ddcfb50850ab287065947b7d8
David Alekna <alekna@live.co.uk>
2020-05-05 15:34:24 +0100

    updates

--- packages/layout/src/components/beta/beta.tsx
+++ packages/layout/src/components/beta/beta.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 import styles from './beta.module.scss';
 import { DocWidth, AppWidth, Flex, P } from '@tpr/core';
 
-type BetaProps = {
-
-}
+type BetaProps = {};
 
 export const Beta: React.FC<BetaProps> = () => {
 	return (
-<DocWidth className={styles.bannerBackground}>
-      <AppWidth>
-        <Flex cfg={{alignItems:"center"}}>
-          <Flex cfg={{bg:"primary.2", color:"background", mr:2, p:1}} >
-            <P cfg={{fontSize:1, p:1}}>BETA</P>
-          </Flex>
-          <P cfg={{fontSize:1}}>
-            This is a new service - your feedback will help us improve it.
-          </P>
-        </Flex>
-      </AppWidth>
-    </DocWidth>
-	)
-}
-
+		<DocWidth className={styles.bannerBackground}>
+			<AppWidth>
+				<Flex cfg={{ alignItems: 'center' }}>
+					<Flex cfg={{ bg: 'primary.2', color: 'background', mr: 2, p: 1 }}>
+						<P cfg={{ fontSize: 1, p: 1 }}>BETA</P>
+					</Flex>
+					<P cfg={{ fontSize: 1 }}>
+						This is a new service - your feedback will help us improve it.
+					</P>
+				</Flex>
+			</AppWidth>
+		</DocWidth>
+	);
+};
