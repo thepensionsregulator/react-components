1c0e60423fded02e005346dc304bc6c4c412e2b7
Ben Walker <ben.walker@tpr.gov.uk>
2020-05-12 11:34:31 +0100

    added layouts exports

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -1,1 +1,9 @@
-export const Superman = 'S';
+export * from './components/address/address';
+export * from './components/beta/beta';
+export * from './components/content/content';
+export * from './components/footer/footer';
+export * from './components/header/header';
+export * from './components/highlight/highlight';
+export * from './components/info/info';
+export * from './components/sidebar/sidebar';
+export * from './components/trustee/trustee';
