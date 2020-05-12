105340072c375dad3d82341c721a56b07238215a
Ben Walker <ben.walker@tpr.gov.uk>
2020-05-12 15:11:11 +0100

    updated highlight to remove doc width from the module

--- packages/layout/src/components/highlight/highlight.tsx
+++ packages/layout/src/components/highlight/highlight.tsx
@@ -12,32 +12,28 @@ export const Highlight: React.FC<HighlightProps> = ({
 	scheme = 'PSR: 12014314',
 }) => {
 	return (
-		<DocWidth>
-			<AppWidth>
-				<Flex cfg={{ flex: '0 0 auto' }}>
-					<Flex
-						className={styles.appTitle}
-						cfg={{ flex: '1 1 auto', alignItems: 'center' }}
-					>
-						<P cfg={{ color: 'background' }}>Scheme return</P>
-					</Flex>
-					<Flex cfg={{ bg: 'accents.4', py: 2 }} className={styles.highlight}>
-						<Flex cfg={{ flex: '1 1 auto', alignItems: 'center', px: 4 }}>
-							<P cfg={{ color: 'background' }}>{title}</P>
-						</Flex>
-						<Flex
-							cfg={{
-								flex: '0 0 auto',
-								alignItems: 'center',
-								justifyContent: 'center',
-							}}
-							className={styles.schemeName}
-						>
-							<P cfg={{ color: 'background' }}>{scheme}</P>
-						</Flex>
-					</Flex>
+		<Flex cfg={{ flex: '0 0 auto' }}>
+			<Flex
+				className={styles.appTitle}
+				cfg={{ flex: '1 1 auto', alignItems: 'center' }}
+			>
+				<P cfg={{ color: 'background' }}>Scheme return</P>
+			</Flex>
+			<Flex cfg={{ bg: 'accents.4', py: 2 }} className={styles.highlight}>
+				<Flex cfg={{ flex: '1 1 auto', alignItems: 'center', px: 4 }}>
+					<P cfg={{ color: 'background' }}>{title}</P>
 				</Flex>
-			</AppWidth>
-		</DocWidth>
+				<Flex
+					cfg={{
+						flex: '0 0 auto',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+					className={styles.schemeName}
+				>
+					<P cfg={{ color: 'background' }}>{scheme}</P>
+				</Flex>
+			</Flex>
+		</Flex>
 	);
 };
