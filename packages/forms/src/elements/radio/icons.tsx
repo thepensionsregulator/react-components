6d68b06a7d4587d562b70f0e3c6fdbd83d2f25eb
Harry Ammon <harry.ammon@tpr.gov.uk>
2020-08-06 15:41:28 +0100

    Add ability to pass classNames as props

--- packages/forms/src/elements/radio/icons.tsx
+++ packages/forms/src/elements/radio/icons.tsx
@@ -29,13 +29,14 @@ export const RadioButtonChecked: React.FC<any> = ({ className }) => {
 	);
 };
 
-export const RadioButtonUnchecked: React.FC = () => {
+export const RadioButtonUnchecked: React.FC<any> = ({className}) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="40"
 			height="40"
 			viewBox="0 0 40 40"
+			className={className}
 		>
 			<g fill="#f5f5f5" stroke="#585858" strokeWidth="1">
 				<circle cx="20" cy="20" r="20" stroke="none" />
