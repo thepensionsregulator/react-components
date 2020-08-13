20f82749324b541e1d5cf24930e75a8bbd9b7322
HarryAmmon <harry_a99@hotmail.co.uk>
2020-08-13 15:21:26 +0100

    Run tests

--- packages/forms/src/validators.ts
+++ packages/forms/src/validators.ts
@@ -12,7 +12,7 @@ export const validateEmail = (email: string) => {
 };
 
 export const validateUkPhone = (phone: string) => {
-	const regex = /^(?=\w*\d)([a-zA-Z\d+\s()+.-]){10,20}$/; 
+	const regex = /^(?=\w*\d)([a-zA-Z\d+\s()+.-]){10,20}$/;
 	return regex.test(String(phone).slice().toLowerCase());
 };
 
