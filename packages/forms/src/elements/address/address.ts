acb728bd2f6e02d8c0a49737edded55cb10c3383
Rick Mason <rick.mason@tpr.gov.uk>
2020-11-05 16:46:20 +0000

    Make the initial view of the address lookup change depending on whether an initial address is provided AB#69404

---
+++ packages/forms/src/elements/address/address.ts
@@ -0,0 +1,10 @@
+export interface Address {
+	addressLine1: string;
+	addressLine2: string;
+	addressLine3: string;
+	postTown: string;
+	county: string;
+	postcode: string;
+	country: string;
+	countryId: number;
+}
--- packages/forms/src/elements/address/addressLookup.tsx
+++ packages/forms/src/elements/address/addressLookup.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { Address } from './address';
 import { EditAddress } from './editAddress';
 import { PostcodeLookup } from './postcodeLookup';
 import { SelectAddress } from './selectAddress';
 
 export type AddressProps = {
+	address?: Address;
 	testId?: string;
 };
 
@@ -13,11 +15,23 @@ enum AddressView {
 	EditAddress,
 }
 
-export const Address: React.FC<AddressProps> = ({ testId }) => {
-	const [addressView, setAddressView] = useState<AddressView>(
-		AddressView.PostcodeLookup,
-	);
+export const AddressLookup: React.FC<AddressProps> = ({ address, testId }) => {
+	// Start in postcode lookup view, unless there's already an address in which case start in edit address view
+	let initialView = AddressView.PostcodeLookup;
+	if (
+		address &&
+		(address.addressLine1 ||
+			address.addressLine2 ||
+			address.addressLine3 ||
+			address.postTown ||
+			address.county ||
+			address.postcode)
+	) {
+		initialView = AddressView.EditAddress;
+	}
+	const [addressView, setAddressView] = useState<AddressView>(initialView);
 
+	// Render a different child component depending on the state
 	switch (addressView) {
 		case AddressView.PostcodeLookup:
 			return (
