728824ad9cde07212a832bdcdc52185f8b4e921a
Rick Mason <rick.mason@tpr.gov.uk>
2020-11-09 19:29:03 +0000

    Add required field validation to the select address field AB#69404

--- packages/forms/src/elements/address/selectAddress.tsx
+++ packages/forms/src/elements/address/selectAddress.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form } from 'react-final-form';
 import { FFSelect } from '../select/select';
 import { P, Button } from '@tpr/core';
 import { Address } from './address';
@@ -39,52 +40,67 @@ export const SelectAddress: React.FC<SelectAddressProps> = ({
 		};
 	});
 
-	let selectedAddressIsValid = true;
-
-	const [selectedAddress, setSelectedAddress] = useState<Address | null>();
+	// Setting a 'valid' object is the only way to control validity of FFSelect.
+	// validate() will run immediately. Initialise to null so that validate() can detect the initial load and set an initial value rather than validating.
+	let [valid, setValid] = useState(null);
 
 	return (
-		<>
-			<fieldset>
-				<legend>{postcodeLookupLabel}</legend>
-				<P>{postcode}</P>
-				<Button
-					onClick={onChangePostcodeClick}
-					appearance="outlined"
-					testId={(testId ? testId + '-' : '') + 'change-postcode'}
-				>
-					{changePostcodeButton}
-				</Button>
-			</fieldset>
-			<FFSelect
-				label={selectAddressLabel}
-				name="address"
-				options={options}
-				inputWidth={6}
-				onChange={(selectedItem) => setSelectedAddress(selectedItem.value)}
-				testId={(testId ? testId + '-' : '') + 'select-address-list'}
-				validate={(value) => {
-					if (value) {
-						selectedAddressIsValid = true;
-						return undefined;
-					} else {
-						selectedAddressIsValid = false;
-						return selectAddressRequiredMessage;
-					}
-				}}
-				notFoundMessage={noAddressesFoundMessage}
-				placeholder={selectAddressPlaceholder}
-			/>
-			<Button
-				testId={(testId ? testId + '-' : '') + 'select-address-button'}
-				onClick={() => {
-					if (selectedAddressIsValid) {
-						onAddressSelected(selectedAddress);
-					}
-				}}
-			>
-				{selectAddressButton}
-			</Button>
-		</>
+		<Form onSubmit={() => {}}>
+			{({ values }) => (
+				<>
+					<fieldset>
+						<legend>{postcodeLookupLabel}</legend>
+						<P>{postcode}</P>
+						<Button
+							onClick={onChangePostcodeClick}
+							appearance="outlined"
+							testId={(testId ? testId + '-' : '') + 'change-postcode'}
+						>
+							{changePostcodeButton}
+						</Button>
+					</fieldset>
+					<FFSelect
+						label={selectAddressLabel}
+						name="address"
+						options={options}
+						inputWidth={6}
+						testId={(testId ? testId + '-' : '') + 'select-address-list'}
+						validate={(value) => {
+							// On initial load, setup the validation object
+							if (!valid) {
+								setValid({ error: '', touched: false });
+								return;
+							}
+
+							// On subsequent runs, update the validation object.
+							// In this case it can only go from invalid (initial load) to valid (address selected).
+							// You can't select an invalid option from the list because there aren't any, and if you don't select one this never runs.
+							if (value) {
+								setValid({ touched: true, error: '' });
+							}
+						}}
+						meta={valid}
+						notFoundMessage={noAddressesFoundMessage}
+						placeholder={selectAddressPlaceholder}
+					/>
+					<Button
+						testId={(testId ? testId + '-' : '') + 'select-address-button'}
+						onClick={() => {
+							// If validate() has set the 'valid' object to a valid state, continue; otherwise set it to an invalid state.
+							if (valid && valid.touched && !valid.error) {
+								onAddressSelected(values.address.value);
+							} else {
+								setValid({
+									touched: true,
+									error: selectAddressRequiredMessage,
+								});
+							}
+						}}
+					>
+						{selectAddressButton}
+					</Button>
+				</>
+			)}
+		</Form>
 	);
 };
