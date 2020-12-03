698266152528487b46d32ccff35a4a9b87add999
HarryAmmon <harry_a99@hotmail.co.uk>
2020-12-03 17:48:52 +0000

    Add ColorFullRange props, remove refactored code AB#73965

--- packages/layout/src/components/poscon/poscon.tsx
+++ packages/layout/src/components/poscon/poscon.tsx
@@ -1,77 +1,57 @@
 import React, { useState } from 'react';
-import { Flex } from '@tpr/core';
+import { ColorsFullRange, Flex } from '@tpr/core';
 import { CrossButton } from '../buttons/buttons';
 import Styles from './poscon.module.scss';
 
 export type PosconProps = {
-	colour?: ColourProps;
-	enableClose: boolean;
+	color?: ColorsFullRange;
+	enableClose?: boolean;
 };
 
-export type ColourProps = {
-	colour?: string;
+export type ColorProps = {
+	color?: ColorsFullRange;
 };
 
 export const Poscon: React.FC<PosconProps> = ({
-	colour,
-	enableClose,
+	color = 'success.1',
+	enableClose = false,
 	children,
 }) => {
-	const bgColour = colour ? colour : 'green';
-	const e = enableClose;
-	console.log(bgColour, e);
 	return enableClose ? (
-		<ClosablePoscon>{children}</ClosablePoscon>
+		<ClosablePoscon color={color}>{children}</ClosablePoscon>
 	) : (
-		<PersistentPoscon>{children}</PersistentPoscon>
+		<PersistentPoscon color={color}>{children}</PersistentPoscon>
 	);
 };
 
-const ClosablePoscon: React.FC<ColourProps> = ({ children }) => {
+const ClosablePoscon: React.FC<ColorProps> = ({ color, children }) => {
 	const [closed, setClosed] = useState<boolean>(false);
 	return !closed ? (
-		<div className={Styles.root} role="alert">
-			<div></div>
-			<div className={Styles.content}>{children}</div>
-			<div className={Styles.enableClose}>
-				<CrossButton
-					colour={'white'}
-					onClick={() => {
-						setClosed(!closed);
-						console.log('clicked');
-					}}
-				/>
-			</div>
+		<div role="alert">
+			<Flex cfg={{ justifyContent: 'space-between', bg: color }}>
+				<div></div>
+				<div className={Styles.content}>{children}</div>
+				<div className={Styles.enableClose}>
+					<CrossButton
+						colour={'white'}
+						onClick={() => {
+							setClosed(!closed);
+						}}
+					/>
+				</div>
+			</Flex>
 		</div>
 	) : (
 		<></>
 	);
 };
 
-const PersistentPoscon: React.FC<ColourProps> = ({ children }) => {
+const PersistentPoscon: React.FC<ColorProps> = ({ color, children }) => {
 	return (
 		<div role="alert">
-			<Flex cfg={{ justifyContent: 'center', bg: 'success.1', py: 8 }}>
+			<Flex cfg={{ justifyContent: 'center', bg: color, py: 8 }}>
 				{children}
 			</Flex>
 		</div>
 	);
 };
-
-// type CrossButtonProps = {
-// 	colour?: ColourProps;
-// 	handleClick: () => void;
-// };
-
-// const CrossButton: React.FC<CrossButtonProps> = ({ colour, handleClick }) => {
-// 	return (
-// 		<div className={Styles.crossButton_root}>
-// 			<Cross colour={'white'} />
-// 			<button
-// 				type={'button'}
-// 				className={Styles.crossButton}
-// 				onClick={handleClick}
-// 			></button>
-// 		</div>
-// 	);
-// };
