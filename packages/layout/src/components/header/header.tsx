84a32647eae42d20c26b90a1fee32f940032b902
Allan Thompson <allan.thompson@tpr.gov.uk>
2020-12-22 10:10:26 +0000

    #75315 Allow logos to have an href

--- packages/layout/src/components/header/header.tsx
+++ packages/layout/src/components/header/header.tsx
@@ -3,14 +3,16 @@ import styles from './header.module.scss';
 import { DocWidth, AppWidth, Flex, Link, P } from '@tpr/core';
 
 type HeaderProps = {
-	logoUrl: string;
+	logoSrc: string;
+	logoHref: string;
 	title: string;
 	onClickSchemeOptions: () => void;
 	onClickLogout: () => void;
 };
 
 export const Header: React.FC<HeaderProps> = ({
-	logoUrl,
+	logoSrc,
+	logoHref,
 	title = 'Exchange - Scheme Return',
 	onClickLogout,
 	onClickSchemeOptions,
@@ -21,7 +23,9 @@ export const Header: React.FC<HeaderProps> = ({
 				<Flex cfg={{ justifyContent: 'space-between', py: 2, pr: 3 }}>
 					<Flex cfg={{ alignItems: 'center' }}>
 						<div className={styles.logo}>
-							<img className={styles.img} src={logoUrl} alt="TPR Logo" />
+							<a href={logoHref} style={{display: "inline-flex"}}>
+								<img className={styles.img} src={logoSrc} alt="TPR Logo" />
+							</a>
 						</div>
 						<P cfg={{ color: 'white', fontWeight: 2 }}>{title}</P>
 					</Flex>
