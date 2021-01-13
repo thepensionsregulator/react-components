cafd3c267195be02e0c42584747591be61b3e16f
Allan Thompson <allan.thompson@tpr.gov.uk>
2021-01-13 16:12:47 +0000

    #78122 - Fix issue where required field validation (using a single "required" message string) is triggered even when using a function for validation

--- packages/forms/src/validation.ts
+++ packages/forms/src/validation.ts
@@ -29,10 +29,10 @@ export function validate(formFields: FieldProps[]) {
 				if (errorMessage) return merge(errors, errorObject);
 			}
 			// typof error is a string and there is no field value, assign error to errors
-			if (
-				(typeof error === 'string' && !fieldValue) ||
+			if (typeof error === 'string' &&
+				(!fieldValue ||
 				(typeof fieldValue === 'string' && !fieldValue.trim()) ||
-				(Array.isArray(fieldValue) && !fieldValue.length)
+				(Array.isArray(fieldValue) && !fieldValue.length))
 			) {
 				// construct object from string with errors
 				const errorObject = qs.parse(`${name}=${error}`, {
