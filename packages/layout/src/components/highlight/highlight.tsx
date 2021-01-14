e09bfb9a9caf01d17fb998b9f60fea236fcf8810
Ben Walker <ben.walker@tpr.gov.uk>
2021-01-14 16:27:20 +0000

    highligh text was grey???

--- packages/layout/src/components/highlight/highlight.tsx
+++ packages/layout/src/components/highlight/highlight.tsx
@@ -18,7 +18,16 @@ export const Highlight: React.FC<HighlightProps> = ({
 			<div className={styles.leftBackground} />
 			<AppWidth className={styles.highlightContent}>
 				<Flex cfg={{ flex: '0 0 auto' }} className={styles.schemeReturn}>
-					<P cfg={{ fontWeight: 4, fontSize: 3, lineHeight: 5 }}>{title}</P>
+					<P
+						cfg={{
+							fontWeight: 4,
+							fontSize: 3,
+							lineHeight: 5,
+							color: 'white',
+						}}
+					>
+						{title}
+					</P>
 				</Flex>
 				<Flex
 					cfg={{
@@ -28,7 +37,13 @@ export const Highlight: React.FC<HighlightProps> = ({
 					}}
 				>
 					<P
-						cfg={{ fontWeight: 4, fontSize: 3, lineHeight: 5, px: 4 }}
+						cfg={{
+							fontWeight: 4,
+							fontSize: 3,
+							lineHeight: 5,
+							px: 4,
+							color: 'white',
+						}}
 						className={styles.truncate}
 					>
 						{name}
@@ -42,7 +57,15 @@ export const Highlight: React.FC<HighlightProps> = ({
 						}}
 						className={styles.psrNumber}
 					>
-						<P cfg={{ fontWeight: 4, fontSize: 3, lineHeight: 5 }}>
+						<P
+							cfg={{
+								fontWeight: 4,
+								fontSize: 3,
+								lineHeight: 5,
+								color: 'white',
+								px: 3,
+							}}
+						>
 							PSR: {psr}
 						</P>
 					</Flex>
