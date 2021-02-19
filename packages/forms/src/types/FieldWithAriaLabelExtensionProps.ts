45040742eb7cedf5bfc5ab14595a16db08abea11
Allan Thompson <allan.thompson@tpr.gov.uk>
2021-02-19 16:08:01 +0000

    AB#82818 - Fix issue incorrectly referencing the Layout package from inside forms

--- packages/forms/src/types/FieldWithAriaLabelExtensionProps.ts
+++ packages/forms/src/types/FieldWithAriaLabelExtensionProps.ts
@@ -1,6 +1,6 @@
-import { RecursivePartial } from '@tpr/layout/lib/components/cards/common/interfaces';
 import { FieldProps } from '../renderFields';
 import { FieldWithAriaLabelExtenstionI18nProps } from './FieldWithAriaLabelExtensionI18nProps';
+import { RecursivePartial } from './RecursivePartial';
 
 export interface FieldWithAriaLabelExtensionProps extends FieldProps {
 	i18n?: RecursivePartial<FieldWithAriaLabelExtenstionI18nProps>;
