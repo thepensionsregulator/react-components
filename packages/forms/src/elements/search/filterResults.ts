38454c66818412862f23188f39635471d4cf3f9e
Victor Rojas <victor.rojas@tpr.gov.uk>
2021-02-24 15:27:25 +0000

    adding searchService prop and some adjustments AB#82807

--- packages/forms/src/elements/search/filterResults.ts
+++ packages/forms/src/elements/search/filterResults.ts
@@ -1,23 +1,30 @@
-export const filterResults = (query, options, keyValue) => {
-  // filter the results
-  const results = options.filter((option) => {
-    return option[keyValue].toLowerCase().indexOf(query.toLowerCase()) !== -1
-  });
+export const filterResults = (query, optionsArray, keyValue) => {
+	// filter the results
+	const results = optionsArray.filter((option) => {
+		return option[keyValue].toLowerCase().indexOf(query.toLowerCase()) !== -1;
+	});
 
-  // generate the array with the results in string format to be displayed
-  let stringResult = [];
+	const stringResult = formatResults(results);
+	console.log('stringResult', stringResult);
 
-  results.forEach((option) => {
-    let newString = ''
-    for (var x in option) {
-      newString += '<p>' + option[x] + '</p>'
-    }
-    stringResult.push(newString);
-  });
+	return {
+		strings: stringResult,
+		objects: results,
+	};
+};
 
-  //return stringResult;
-  return {
-    strings: stringResult,
-    objects: results
-  }
+export const formatResults = (results:any) => {
+	// generate the array with the results in string format to be displayed
+	let stringResult = [];
+
+	results.forEach((option) => {
+		let newString = '';
+		for (var x in option) {
+			newString += '<p>' + option[x] + '</p>';
+		}
+		stringResult.push(newString);
+	});
+
+	return stringResult;
 }
+
--- packages/forms/src/elements/search/search.tsx
+++ packages/forms/src/elements/search/search.tsx
@@ -1,119 +1,162 @@
-import React, { useState, useEffect, useRef } from 'react';
-//import Downshift, { DownshiftProps } from 'downshift';
+import React, { useState, useEffect } from 'react';
 import { Field, FieldRenderProps } from 'react-final-form';
 import { Flex } from '@tpr/core';
 import { StyledInputLabel, InputElementHeading } from '../elements';
-//import { Input } from '../input/input';
-import { FieldProps, FieldOptions, FieldExtraProps } from '../../renderFields';
+import { FieldProps, FieldExtraProps } from '../../renderFields';
+import AccessibilityHelper from '../accessibilityHelper';
 import Autocomplete from 'accessible-autocomplete/react';
-import { filterResults } from './filterResults';
+import { filterResults, formatResults } from './filterResults';
 import styles from './search.module.scss';
 
-interface SearchProps extends FieldExtraProps {
-	options?: FieldOptions[];
+interface SearchProps extends FieldRenderProps<string>, FieldExtraProps {
+	optionsArray?: any[];
 	notFoundMessage?: string;
 	keyValue: string;
+	searchService?: (x:string) => Promise<any>;
 	callback?: Function;
 }
 
-const Search: React.FC<SearchProps & FieldRenderProps<string>> = React.memo(({
-	options,
-	label,
-	required,
-	hint,
-	meta,
-	notFoundMessage = 'Your search criteria has no match',
-	testId = 'search',
-	placeholder,
-	inputWidth,
-	cfg,
-	keyValue,
-	callback,
-	name,
-	...rest
-}) => {
-	const [panelVisible, setPanelVisible] = useState(false);
-	const [classes, setClasses] = useState(styles.autocomplete);
-	//const [inputValue, setInputValue] = useState(undefined);
-	const [optionsArrayStrings, setOptionsArrayStrings] = useState([]);
-	const [optionsArrayObjects, setOptionsArrayObjects] = useState([]);
+type panelVisibility = 
+	'visible'|
+	'hidden'|
+	'complete'
+;
 
-	const showResults = (query) => {
-		// set panelVisible=true in case it was false
-		if(!panelVisible) setPanelVisible(true);
+const Search: React.FC<SearchProps> = React.memo(
+	({
+		optionsArray = [],
+		label,
+		required,
+		hint,
+		meta,
+		notFoundMessage = 'Your search criteria has no match',
+		testId = 'search',
+		placeholder,
+		inputWidth,
+		cfg,
+		keyValue,
+		callback,
+		searchService,
+		name,
+		...rest
+	}) => {
+		const [panelVisible, setPanelVisible] = useState<panelVisibility>('hidden');
+		const [classes, setClasses] = useState(styles.autocomplete);
+		const [optionsArrayStrings, setOptionsArrayStrings] = useState([]);
+		const [optionsArrayObjects, setOptionsArrayObjects] = useState([]);
 
-		const resultsFiltered = filterResults(query, options, keyValue);
-
-		setOptionsArrayObjects(resultsFiltered.objects);
-		setOptionsArrayStrings(resultsFiltered.strings);
-
-		return resultsFiltered.strings;
-	}
-
-	const getResults = (query, syncResults) => {
-		syncResults(query
-			? showResults(query)
-			: []
-		);
-	}
-
-	const toggleResultsPanel = () => {
-		const newClasses = panelVisible ? styles.autocomplete + ' ' + styles.panelVisible : styles.autocomplete;
-		if(newClasses !== classes) setClasses(newClasses); 
-	}
-
-	const chooseOption = (value) => {
-		panelVisible && setPanelVisible(false);
-		//setInputValue(value);
-		if(value) {
-			console.log('chooseOption - value', value);
-			const objectSelected = optionsArrayObjects[optionsArrayStrings.indexOf(value)];
-
-			callback(objectSelected);
+		const setGlobalArrays = (resultsFiltered) => {			
+			setOptionsArrayObjects(resultsFiltered.objects);
+			setOptionsArrayStrings(resultsFiltered.strings);
 		}
-	}
 
-	useEffect(() => {
-		toggleResultsPanel();
-	}, [panelVisible]);
+		const showResults = (query) => {
+			// Function that filters the results when the options are passed directly as an array of values
+			console.log('showResults');
+			// set panelVisible=visible in case it was hidden
+			if (panelVisible == 'hidden') setPanelVisible('visible');
 
-	const accessibleInput = useRef(null);
+			const resultsFiltered = filterResults(query, optionsArray, keyValue);
+			console.log('resultsFiltered', resultsFiltered);
 
-	return (
-				<div className={classes}>
-					<StyledInputLabel
-						element="div"
-						isError={meta && meta.touched && meta.error}
-						cfg={Object.assign({ flexDirection: 'column' }, cfg)}
-						{...rest}
+			setGlobalArrays(resultsFiltered);
+			return resultsFiltered.strings;
+		};
+
+		const useSearchService = async (query:string) => {
+			// Function that process the results when receiving 'searchService' for making the search
+			console.log('useSearchService');
+			let resutsFormatted = [];
+
+			await searchService(query).then(resultsFiltered => {
+				resutsFormatted = formatResults(resultsFiltered);
+				setGlobalArrays(resultsFiltered);
+				console.log('resultsFiltered', resultsFiltered);
+				// set panelVisible=visible in case it was hidden
+				if (panelVisible == 'hidden') setPanelVisible('visible');
+				console.log('resutsFormatted', resutsFormatted);
+			});
+			//const resutsFormatted = formatResults(resultsFiltered)
+			console.log('resutsFormatted', resutsFormatted);
+
+			return resutsFormatted;
+		}
+
+		const getResults = (query, syncResults) => {
+			syncResults(query ? searchService ? useSearchService(query) : showResults(query) : []);
+		};
+
+		const toggleResultsPanel = () => {
+			console.log('toggleResultsPanel');
+			const newClasses = panelVisible == 'complete'
+				? styles.autocomplete + ' ' + styles.hide
+				: panelVisible == 'visible'
+					? styles.autocomplete + ' ' + styles.panelVisible
+					: styles.autocomplete;
+			if (newClasses !== classes) setClasses(newClasses);
+		};
+
+		const chooseOption = (value: string) => {
+			console.log('chooseOption');
+			if (value) {
+				// set panelVisible=complete when option has been selected
+				setPanelVisible('complete');
+				const objectSelected =
+					optionsArrayObjects[optionsArrayStrings.indexOf(value)];
+
+				callback(objectSelected);
+			}
+		};
+
+		useEffect(() => {
+			console.log('useEffect');
+			toggleResultsPanel();
+		}, [panelVisible]);
+
+		const helper = new AccessibilityHelper(rest['id'], !!label, !!hint);
+
+		return (
+			<div className={classes}>
+				<StyledInputLabel
+					element="label"
+					isError={meta && meta.touched && meta.error}
+					cfg={Object.assign({ flexDirection: 'column' }, cfg)}
+					{...rest.ariaLabel}
+				>
+					<InputElementHeading
+						label={label}
+						required={required}
+						hint={hint}
+						meta={meta}
+						accessibilityHelper={helper}
+					/>
+					<Flex
+						cfg={{ width: inputWidth, flexDirection: 'column' }}
+						className={styles.relative}
 					>
-						<InputElementHeading
-							label={label}
-							required={required}
-							hint={hint}
-							meta={meta}
+						<Autocomplete
+							name={name}
+							source={getResults}
+							onConfirm={chooseOption}
+							showAllValues={false}
+							minLength={3}
+							tNoResults={() => notFoundMessage}
+							placeholder={placeholder}
 						/>
-						<Flex cfg={{ width: inputWidth, flexDirection: 'column' }} className={styles.relative}>
-							{/* <Input
-									type="text"
-									className={styles.hiddenInput}
-									value={inputValue}
-								/> */}
-							<Autocomplete
-								ref={accessibleInput}
-								name={name}
-								source={getResults}
-								onConfirm={chooseOption}
-								showAllValues={false}
-								tNoResults={() => notFoundMessage}
-								placeholder={placeholder}
-							/>
-						</Flex>
-					</StyledInputLabel>
-				</div>
+					</Flex>
+				</StyledInputLabel>
+			</div>
+		);
+	},
+);
+
+export const FFSearch: React.FC<
+	FieldProps & Omit<SearchProps, 'children'>
+> = React.memo((fieldProps) => {
+	return (
+		<Field
+			{...fieldProps}
+			render={(props) => <Search {...props} name={fieldProps.name} />}
+		/>
 	);
 });
-
-export const FFSearch: React.FC<FieldProps & Omit<SearchProps, 'children'>> = React.memo((
-	fieldProps,
-) => <Field component={Search} {...fieldProps} />);
