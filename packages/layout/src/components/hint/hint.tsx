aebb75d8cc9ac6ddeb7ef699176b22becc1e1f7f
George Mukwewa <George.Mukwewa@tpr.gov.uk>
2021-03-03 19:18:56 +0000

    AB#86122 fixed the issue with links inside help text hints

--- packages/layout/src/components/hint/hint.tsx
+++ packages/layout/src/components/hint/hint.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@tpr/core';
 import React from 'react';
 import Styles from './hint.module.scss';
+
 type HintProps = {
 	children?: any;
 	expanded?: boolean;
 }
+
 export const Hint = (props: HintProps) => (
 	<Flex cfg={{ pl: 3 }} className={props.expanded ? Styles.root : Styles.collapsed}>
 		{props.children}
