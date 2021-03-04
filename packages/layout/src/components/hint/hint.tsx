ea42acd13b803182bbeda75e3cc1b9b94ee0ddf9
Victor Rojas <victor.rojas@tpr.gov.uk>
2021-03-04 13:03:07 +0000

    prettier format AB#85291

--- packages/layout/src/components/hint/hint.tsx
+++ packages/layout/src/components/hint/hint.tsx
@@ -5,10 +5,13 @@ import Styles from './hint.module.scss';
 type HintProps = {
 	children?: any;
 	expanded?: boolean;
-}
+};
 
 export const Hint = (props: HintProps) => (
-	<Flex cfg={{ pl: 3 }} className={props.expanded ? Styles.root : Styles.collapsed}>
+	<Flex
+		cfg={{ pl: 3 }}
+		className={props.expanded ? Styles.root : Styles.collapsed}
+	>
 		{props.children}
 	</Flex>
 );
