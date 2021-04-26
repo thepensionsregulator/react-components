5aa171454bbc4783dc1b6e29d6a0f572f20ee85b
George Mukwewa <George.Mukwewa@tpr.gov.uk>
2021-04-26 05:56:08 +0100

    AB#88254 applied cancel functionality changes to all cards

--- packages/layout/src/components/cards/employer/context.tsx
+++ packages/layout/src/components/cards/employer/context.tsx
@@ -72,13 +72,7 @@ export const EmployerProvider = ({
 		},
 	});
 
-	const fwdValues = {
-		current,
-		send,
-		showStatutoryEmployerSection,
-		i18n,
-		...rest,
-	};
+	const fwdValues = { current, send, showStatutoryEmployerSection, i18n, ...rest };
 	const ui = typeof children === 'function' ? children(fwdValues) : children;
 	return (
 		<EmployerContext.Provider value={fwdValues}>{ui}</EmployerContext.Provider>
