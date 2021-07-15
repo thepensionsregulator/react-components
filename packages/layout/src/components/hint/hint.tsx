49cfc89859807b7be2f40e164c95da90bce1b9f7
Rick Mason <rick.mason@tpr.gov.uk>
2021-07-15 09:57:32 +0100

    Add default spacing below button, hint and helplink components. Ensure className is supported to override if required. AB#95400

--- packages/layout/src/components/hint/hint.tsx
+++ packages/layout/src/components/hint/hint.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Flex } from '@tpr/core';
 import Styles from './hint.module.scss';
+import { render } from 'react-dom';
 
 type HintProps = {
+	className?: string;
 	children?: any;
 	expanded?: boolean;
 };
 
-export const Hint = (props: HintProps) => (
-	<Flex
-		cfg={{ pl: 3 }}
-		className={props.expanded ? Styles.root : Styles.collapsed}
-	>
-		{props.children}
-	</Flex>
-);
+export const Hint = (props: HintProps) => {
+	let className = props.expanded ? Styles.root : Styles.collapsed;
+	if (props.className) {
+		className += ' ' + props.className;
+	}
+	return (
+		<Flex cfg={{ pl: 3 }} className={className}>
+			{props.children}
+		</Flex>
+	);
+};
