fad7fd26a4f18fe64f49237aa246a587b571d5ac
Tobias Bunyan <Tobias.Bunyan@tpr.gov.uk>
2021-08-17 15:24:34 +0100

    Put the screen size calcualtion function in here. Whilst the breakpoint values cannot be pulled in automatically, it should at least live somewhere that if those values changed it should get noticed.
    For:
    #103272

--- packages/theming/src/theming.tsx
+++ packages/theming/src/theming.tsx
@@ -11,3 +11,25 @@ export const CssResets: React.FC = ({ children }) => (
 export const EditorFonts: React.FC = ({ children }) => {
 	return <div className={editor.editorFontStack}>{children}</div>;
 };
+
+export const getBreakpoint = () => {
+		let fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0]),
+	 break1em = 48,
+	 break2em = 64,
+	 break3em = 90,
+	 break4em = 114,
+	 breakpoint ='',
+	 width = window.innerWidth / fontSize;
+ if (width < break1em) {
+	 breakpoint = '1. < ' + break1em + 'em';
+ } else if(width >= break1em && width < break2em) {
+	 breakpoint = '2. ' + break1em + 'em - ' + break2em + 'em';
+ } else if(width >= break2em && width < break3em) {
+	 breakpoint = '3. ' + break2em + 'em - ' + break3em + 'em';
+ } else if(width >= break3em && width < break4em) {
+	 breakpoint = '4. ' + break3em + 'em - ' + break4em + 'em'; 
+ } else if(width >= break4em) {
+	 breakpoint = '5. >= ' + break4em + 'em';
+ }
+ return breakpoint;
+}
