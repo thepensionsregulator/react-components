3b5a411cc3a499e6c166d240913fc85d12722575
Victor Rojas <victor.rojas@tpr.gov.uk>
2021-09-01 14:43:47 +0100

    AB#106837 refactoring 'UnderlinedButton' and adding new prop 'notHeading' allow rendering H3,H4 or P

--- packages/layout/src/components/cards/components/button.tsx
+++ packages/layout/src/components/cards/components/button.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, MutableRefObject } from 'react';
-import { Flex, H3, H4 } from '@tpr/core';
-import styles from './button.module.scss';
-import { EditArrowUp, EditArrowDown } from './arrowButton';
+import { Flex, H3, H4, P } from '@tpr/core';
 import { ArrowDown, ArrowUp } from '@tpr/icons';
+import { EditArrowUp, EditArrowDown } from './arrowButton';
+import styles from './button.module.scss';
 
 interface UnderlinedButtonProps {
 	isOpen?: boolean;
@@ -10,8 +10,9 @@ interface UnderlinedButtonProps {
 	tabIndex?: number;
 	isEditButton?: boolean;
 	onCollapseCallback?: () => void;
-	btnRef?: MutableRefObject<any>;
+	buttonRef?: MutableRefObject<any>;
 	isMainHeading?: boolean;
+	notHeading?: boolean;
 }
 
 /*
@@ -21,7 +22,7 @@ interface UnderlinedButtonProps {
 
 	To solve this, 2 new properties need to be passed to the Underlinedbutton component.
 
-	btnRef: reference received for buttons from the 'Preview' view, but only for those which are part of the 'content' section (not the Toolbar buttons).
+	buttonRef: reference received for buttons from the 'Preview' view, but only for those which are part of the 'content' section (not the Toolbar buttons).
 
 	onCollapseCallback: callback function to be called when collapsing an editing section. This function is used to return the focus to the button from 'Preview'.
 	*/
@@ -33,28 +34,60 @@ export const UnderlinedButton: React.FC<UnderlinedButtonProps> = ({
 	tabIndex,
 	isEditButton,
 	onCollapseCallback,
-	btnRef,
+	buttonRef,
 	isMainHeading = false,
+	notHeading = false,
 }) => {
-	if (typeof onClick === 'undefined') {
-		return (
-			<div className={styles.buttonPlaceholder}>
-				<Flex cfg={{ flex: '0 0 auto', alignItems: 'center' }}>
-					{isMainHeading ? (
-						<H3 cfg={{ fontWeight: 3 }}>{children}</H3>
-					) : (
-						<H4>{children}</H4>
-					)}
-				</Flex>
-			</div>
-		);
-	}
+	const NotClickableButton: React.FC = () => (
+		<div className={styles.buttonPlaceholder}>
+			<Flex cfg={{ flex: '0 0 auto', alignItems: 'center' }}>
+				{isMainHeading ? (
+					<H3 cfg={{ fontWeight: 3, fontSize: 3 }}>{children}</H3>
+				) : (
+					<H4 cfg={{ fontSize: 2 }}>{children}</H4>
+				)}
+			</Flex>
+		</div>
+	);
 
-	const noToolbarBtnRef = useRef(null);
-	useEffect(() => {
-		isOpen && noToolbarBtnRef && noToolbarBtnRef.current.focus();
-		!isOpen && onCollapseCallback && onCollapseCallback();
-	}, [isOpen]);
+	const ClickableButton: React.FC = () => (
+		<button
+			className={styles.button}
+			onClick={onClick}
+			aria-expanded={isOpen}
+			tabIndex={tabIndex}
+			ref={buttonRef ? buttonRef : noToolbarButtonRef}
+		>
+			<Flex
+				className={styles.arrowSpacing}
+				cfg={{ flex: '0 0 auto', alignItems: 'center' }}
+			>
+				{children}
+				{getAppropriateButton()}
+			</Flex>
+		</button>
+	);
+
+	const MainHeadingButton: React.FC = () => (
+		<H3 cfg={{ fontWeight: 3, fontSize: 3 }}>
+			<ClickableButton />
+		</H3>
+	);
+
+	const HeadingButton: React.FC = () => (
+		<H4 cfg={{ fontSize: 2 }}>
+			<ClickableButton />
+		</H4>
+	);
+
+	const NotHeadingButton: React.FC = () => (
+		<P cfg={{ fontWeight: 3 }}>
+			<ClickableButton />
+		</P>
+	);
+
+	const noToolbarButtonRef = useRef(null);
+
 	const getAppropriateButton = () => {
 		if (isOpen && isEditButton) {
 			return <EditArrowUp width="24px" fill={styles.arrowColor} />;
@@ -69,25 +102,22 @@ export const UnderlinedButton: React.FC<UnderlinedButtonProps> = ({
 		}
 	};
 
+	useEffect(() => {
+		isOpen && noToolbarButtonRef && noToolbarButtonRef.current.focus();
+		!isOpen && onCollapseCallback && onCollapseCallback();
+	}, [isOpen]);
+
 	return (
-		<button
-			className={styles.button}
-			onClick={onClick}
-			aria-expanded={isOpen}
-			tabIndex={tabIndex}
-			ref={btnRef ? btnRef : noToolbarBtnRef}
-		>
-			<Flex
-				className={styles.arrowSpacing}
-				cfg={{ flex: '0 0 auto', alignItems: 'center' }}
-			>
-				{isMainHeading ? (
-					<H3 cfg={{ fontWeight: 3 }}>{children}</H3>
-				) : (
-					<H4>{children}</H4>
-				)}
-				{getAppropriateButton()}
-			</Flex>
-		</button>
+		<>
+			{typeof onClick === 'undefined' ? (
+				<NotClickableButton />
+			) : isMainHeading ? (
+				<MainHeadingButton />
+			) : notHeading ? (
+				<NotHeadingButton />
+			) : (
+				<HeadingButton />
+			)}
+		</>
 	);
 };
