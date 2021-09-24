faf06acc112c6abfb7c1f6393dd124854f7f0590
Rick Mason <rick.mason@tpr.gov.uk>
2021-09-24 12:04:24 +0100

    Make the header responsive AB#109282

--- packages/layout/src/components/highlight/highlight.tsx
+++ packages/layout/src/components/highlight/highlight.tsx
@@ -1,74 +1,26 @@
 import React from 'react';
-import { AppWidth, DocWidth, Flex, Span } from '@tpr/core';
+import { AppWidth, DocWidth, Flex } from '@tpr/core';
 import styles from './highlight.module.scss';
 
 type HighlightProps = {
-	title?: string;
+	context?: string;
 	name: string;
-	psr: string;
+	reference: string;
 	testId?: string;
 };
 export const Highlight: React.FC<HighlightProps> = ({
-	title = 'Scheme return',
-	name = '',
-	psr = '',
+	context,
+	name,
+	reference,
 }) => {
 	return (
 		<DocWidth className={styles.highlight}>
-			<div className={styles.leftBackground} />
 			<AppWidth className={styles.highlightContent}>
-				<Flex cfg={{ flex: '0 0 auto' }} className={styles.schemeReturn}>
-					<Span
-						cfg={{
-							fontWeight: 4,
-							fontSize: 3,
-							lineHeight: 5,
-							color: 'white',
-							ml: 6,
-						}}
-					>
-						{title}
-					</Span>
-				</Flex>
-				<Flex
-					cfg={{
-						flex: '1 1 auto',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-					}}
-				>
-					<Span
-						cfg={{
-							fontWeight: 4,
-							fontSize: 3,
-							lineHeight: 5,
-							px: 4,
-							color: 'white',
-						}}
-						className={styles.truncate}
-					>
-						{name}
-					</Span>
-					<Flex
-						cfg={{
-							flex: '0 0 auto',
-							alignItems: 'center',
-							justifyContent: 'flex-end',
-						}}
-						className={styles.psrNumber}
-					>
-						<div className={styles.separator}></div>
-						<Span
-							cfg={{
-								fontWeight: 4,
-								fontSize: 3,
-								lineHeight: 6,
-								color: 'white',
-								px: 6,
-							}}
-						>
-							PSR: {psr}
-						</Span>
+				<Flex className={styles.context}>{context}</Flex>
+				<Flex className={styles.container}>
+					<Flex className={styles.name}>{name}</Flex>
+					<Flex className={styles.reference}>
+						<Flex>{reference}</Flex>
 					</Flex>
 				</Flex>
 			</AppWidth>
