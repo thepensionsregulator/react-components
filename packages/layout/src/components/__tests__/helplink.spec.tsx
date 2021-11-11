e1d8519f10da9580f93d50b0408bfd3f4bd53954
Rick Mason <rick.mason@tpr.gov.uk>
2021-11-11 10:01:00 +0000

    Add tests for help text AB#114507

--- packages/layout/src/components/__tests__/helplink.spec.tsx
+++ packages/layout/src/components/__tests__/helplink.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { HelpLink } from '../helplink/helplink';
+import { axe } from 'jest-axe';
+import { formSetup } from '@tpr/forms';
 
 describe('HelpLink', () => {
 	test('displays title', () => {
@@ -16,5 +18,23 @@ describe('HelpLink', () => {
 		expect(renderOutput.container.textContent).toContain(
 			'This is some HelpLink content',
 		);
+	});
+	test('uses type=button', async () => {
+		const renderOutput = render(
+			<HelpLink title="HelpLink Title">This is some HelpLink content</HelpLink>,
+		);
+		const button = await renderOutput.findByText('HelpLink Title');
+		expect(button).toHaveAttribute('type', 'button');
+	});
+	test('passes accessibility checks', async () => {
+		const { container } = formSetup({
+			render: (
+				<HelpLink title="HelpLink Title">
+					This is some HelpLink content
+				</HelpLink>
+			),
+		});
+		const results = await axe(container);
+		expect(results).toHaveNoViolations();
 	});
 });
